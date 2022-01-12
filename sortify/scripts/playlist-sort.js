943472b87c828b2afac9c502c9cdda97de2dffcb
lamyeemanick <nick.lam-yee-man@epitech.eu>
2022-01-12 23:49:33 +0400

    add local torage in json by playlsit and need to start scoring + check for reset the playlsit or not when loading

--- sortify/scripts/playlist-sort.js
+++ sortify/scripts/playlist-sort.js
@@ -1,52 +1,85 @@
-//var playlistID = "5T3vz9WJ41rjOOpJ6AAURV"
-var playlistID = "3riXgB8dCeuM4LnUXOX0cy"
+var playlistID = "61f45XS8TkadYTBC6Kokrm"
+//var playlistID = "3riXgB8dCeuM4LnUXOX0cy"
 var offset = 0;
+var it = 1;
+
+function updateLocalStorage(element)
+{
+    var ls = localStorage.getItem("playlist-json");
+    var psize = document.getElementById("playlist-size");
+
+    var jsontext = "\"" + it + "\" : { \"track-id\" : \"" + element.track.id + "\", \"score\" : \"0\" } ";
+
+    if (psize.innerText != it)
+        jsontext += ",";
+    else
+        jsontext += "}";
+
+    ls += jsontext;
+    localStorage.setItem("playlist-"+playlistID, ls);
+}
 
 function updateHTML(element)
 {
+    /* create ligne of the table */
     var tr = document.createElement('tr');
+    var num = document.createElement('td');
     var name = document.createElement('td');
     var artists = document.createElement('td');
     var score = document.createElement('td');
 
+    /* position number in playlist */
+    num.innerHTML = it;
+
+    /* title */
     name.innerHTML = element.track.name;
+
+    /* artists */
     element.track.artists.forEach(artist => {
         if (artists.innerHTML == "")
             artists.innerHTML = artist.name;
         else
             artists.innerHTML += ", " + artist.name;
     })
+    /* score */
     score.innerHTML = "0";
     
+    /* push ligne of the table */
+    tr.appendChild(num);
     tr.appendChild(name);
     tr.appendChild(artists);
     tr.appendChild(score);
     return tr;
 }
 
-function fillPlaylistTracks(playlist, plist, spotifyApi)
+function fillPlaylistTracks(plist, spotifyApi)
 {
+    /* create options for offset in request */
     const opt = new Object();
     opt.offset = offset;
 
+    /* request */
     spotifyApi.getPlaylistTracks(playlistID, opt, (err, data) => new Promise((resolve, reject) => {
         if (err)
             reject(err);
         else
             resolve(data.items);
     }).then((items) => {
+        /* push each song in the table */
         items.forEach(element => {
-            playlist.push(element)
             console.log(element);
-
+            /* change the HTML */
             plist.appendChild(updateHTML(element));
+            /* change the LocalStorage */
+            updateLocalStorage(element);
+            it += 1;
         });
     }));
 }
 
-
 function getPlaylistSize(spotifyApi, playlistSize)
 {
+    /* create options */
     const opt = new Object();
     opt.offset = 0;
 
@@ -56,34 +89,40 @@ function getPlaylistSize(spotifyApi, playlistSize)
         else
             resolve(data.total);
     }).then((total) => {
+        /* change the tag p with the size of the playlist */
         playlistSize.innerHTML = total;
     }));
 }
 
 function getPlaylist()
 {
-    /** create a SpotifyWebApi class to make request later + set the access token **/
+    /* create a SpotifyWebApi class to make request later*/
     var spotifyApi = new SpotifyWebApi();
-    spotifyApi.setAccessToken(localStorage.getItem("accessToken"));
-    
-    var playlist = [];
 
+    /* set the access token */
+    spotifyApi.setAccessToken(localStorage.getItem("accessToken"));
+
+    /* get the tag p and table to modify next */
     var psize = document.getElementById("playlist-size");
     var plist = document.getElementById("playlist-list");
 
-
+    /* listen the tag p when modified */
     psize.addEventListener('DOMSubtreeModified', function() {
-        fillPlaylistTracks(playlist, plist, spotifyApi);
+        fillPlaylistTracks(plist, spotifyApi);
     })
     
+    /* listen the tag table when modified */
     plist.addEventListener('DOMSubtreeModified', function() {
-        if (psize.innerHTML > offset) {
+        if (psize.innerHTML > offset && it % 100 == 0) {
             offset += 100;
-            fillPlaylistTracks(playlist, plist, spotifyApi);
-        } else {
-            console.log(playlist);        }
+            fillPlaylistTracks(plist, spotifyApi);
+        }
     })
 
-    /** get size of the playlist **/
+    /* reset local storage content */
+    localStorage.removeItem("playlist-"+playlistID);
+    localStorage.setItem("playlist-"+playlistID, "{");
+
+    /* get size of the playlist */
     getPlaylistSize(spotifyApi, psize);
 }
--- sortify/scripts/spotify-connect.js
+++ sortify/scripts/spotify-connect.js
@@ -1,17 +1,17 @@
-function onLoad()
+function setAccessToken()
 {
     var data = "grant_type=client_credentials";
-
+    
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
-
+    
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
             let accessToken = this.responseText.split(':')[1].split("\"")[1];
             localStorage.setItem("accessToken", accessToken);
         }
     });
-
+    
     xhr.open("POST", "https://accounts.spotify.com/api/token");
     xhr.setRequestHeader("Authorization", "Basic MTMyMzk3OTQ1Zjk4NGFhZmJjNzZmZDUyOTY3ZDJlN2E6MTc2OWI2OGViYTA5NGJlM2I4NTE1NDkzNzEwYmM0YTg=");
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -19,4 +19,9 @@ function onLoad()
      **  xhr.setRequestHeader("Cookie", "__Host-device_id=AQB5MM_VrrlXFNDXUpktnPXGUTDVhbS1E8oXsJz-NuRx0mZuZ0cH8zab52IJb0FVDAQb5R3Pl-xk9AvxtXImNWHsaGxMop0tBSw; sp_tr=false");
      */
     xhr.send(data);
+}
+
+function onLoad()
+{
+    setAccessToken();
 }
